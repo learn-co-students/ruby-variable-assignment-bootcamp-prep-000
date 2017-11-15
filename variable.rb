a0f24abdfed4b728edd299dc7725400a90a46c67
Stavroula Guerrero <4stacyg@optonline.net>
2017-11-15 17:18:56 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,2 +1,3 @@
- # Define a local variable called 'greeting',
+ greeting = "Hello World"
+ puts greeting# Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
