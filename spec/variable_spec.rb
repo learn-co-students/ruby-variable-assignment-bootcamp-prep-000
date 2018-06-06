7eb7ad0141e61dbda87229c4f1a61d075626f82b
max behling <elmachomax123@gmail.com>
2018-06-06 20:06:40 +0000

    Done.

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -9,4 +9,8 @@ describe "./variable.rb" do
 
 end
 
+<<<<<<< HEAD
 var greeting = 'Hello World';
+=======
+var greeting = 'Hello World'
+>>>>>>> 934dc8be4cadcb0b7f47acc30abcf408dc2c9edd
--- variable.rb
+++ variable.rb
@@ -2,5 +2,5 @@
  # give it the value of the string "Hello World"
 
 
-var greeting;
-greeting = "Hello World";
+var greeting = "Hello World"
+puts greeting
