6f2089e4bb43f02d9d8cb7bf065c2936ce23a878
Derrick Skipwith <dskipwith15@gmail.com>
2018-07-05 14:51:46 +0000

    Done.

--- spec/variable_spec.rb
+++ spec/variable_spec.rb
@@ -1,4 +1,5 @@
-# Code your solution in variable.rb
+greeting = "Hello World"
+puts "{greeting}"
 describe "./variable.rb" do
 
   it "defined a local variable called greeting and set it equal to 'Hello World'" do
